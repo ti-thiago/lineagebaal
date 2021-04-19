af5acb1b9fb25a17cc07a97222734262fb0e1b8f
ti-thiago <54894891+ti-thiago@users.noreply.github.com>
2021-04-19 20:29:41 -0300

    Commit cadastro usuário

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,13 +4,20 @@ import "./styled.css";
 import LogoWhite from "../../images/logo-white.png";
 
 import LogoFb from "../../images/facebook-button.png";
+import api from "../../services/api";
+import { Form } from "@unform/web";
+import Input from "../Input";
+import toast, { Toaster } from "react-hot-toast";
 
 const Header: React.FC = () => {
+  const formRef = useRef();
   const [loading, setLoading] = useState(false);
 
   const [displayLogin, setDisplayLogin] = useState(false);
+  const [displayRegister, setDisplayRegister] = useState(false);
   const [displayCommunity, setDisplayCommunity] = useState(false);
   const [displayGame, setDisplayGame] = useState(false);
+  const [resp, setResp] = useState<string>("");
   const toggleDropDown = useCallback((e: any) => {
     if (!e.target.closest("[data-class]")) {
       setDisplayCommunity(false);
@@ -19,6 +26,15 @@ const Header: React.FC = () => {
       setDisplayGame(false);
     } else if (!!displayGame) {
       setDisplayCommunity(false);
+    }
+  }, []);
+
+  const handleSubmit = useCallback(async (data: any) => {
+    try {
+      const resp = await api.post("/", data);
+      toast.success(resp.data);
+    } catch (err) {
+      toast.error(`Ocorreu um erro ao cadastrar! ${err.message} `);
     }
   }, []);
 
@@ -106,9 +122,9 @@ const Header: React.FC = () => {
             <a
               href="#modal-login"
               className="loginButton"
-              onClick={() => setDisplayLogin(true)}
+              onClick={() => setDisplayRegister(true)}
             >
-              Log In
+              Register
             </a>
             <a href="" className="downloadButton bright">
               Download
@@ -120,43 +136,88 @@ const Header: React.FC = () => {
           <a href="" className="dc"></a>
         </div>
       </div>
-      <div
-        id="modal-login"
-        className="modal_div t-center "
-        style={{ display: "none" }}
-      >
-        <div className="modal_close" onClick={() => setDisplayLogin(false)}>
-          <span></span>
-          <span></span>
+      {/* {displayLogin && (
+        <div
+          id="modal-login"
+          className="modal_div t-center "
+          style={{ display: "none" }}
+        >
+          <div className="modal_close" onClick={() => setDisplayLogin(false)}>
+            <span></span>
+            <span></span>
+          </div>
+          <h1>Login</h1>
+          <a href="#">
+            <img src={LogoFb} alt="" />
+          </a>
+          <div className="or">Or</div>
+          <form className="form-width">
+            <p>
+              <input  placeholder="Login" />
+            </p>
+            <p>
+              <input type="password" placeholder="Password" />
+            </p>
+            <p>
+              <button>ok</button>
+            </p>
+          </form>
+          <div className="formlinks">
+            <p>
+              <a href="">Forgot your password ?</a>
+            </p>
+            <p>
+              Dont`t have an account ?{" "}
+              <a href="" className="reg">
+                Register
+              </a>
+            </p>
+          </div>
         </div>
-        <h1>Login</h1>
-        <a href="#">
-          <img src={LogoFb} alt="" />
-        </a>
-        <div className="or">Or</div>
-        <form className="form-width">
-          <p>
-            <input type="text" placeholder="Login" />
-          </p>
-          <p>
-            <input type="password" placeholder="Password" />
-          </p>
-          <p>
-            <button>ok</button>
-          </p>
-        </form>
-        <div className="formlinks">
-          <p>
-            <a href="">Forgot your password ?</a>
-          </p>
-          <p>
-            Dont`t have an account ?{" "}
-            <a href="" className="reg">
-              Register
-            </a>
-          </p>
+      )} */}
+      {displayRegister && (
+        <div id="modal-login" className="modal_div t-center ">
+          <div
+            className="modal_close"
+            onClick={() => setDisplayRegister(false)}
+          >
+            <span></span>
+            <span></span>
+          </div>
+          <h1>Register</h1>
+          <a href="#">
+            <img src={LogoFb} alt="" />
+          </a>
+          <div className="or">Or</div>
+          <Form className="form-width" onSubmit={handleSubmit}>
+            <p>
+              <Input name="email" placeholder="E-mail" />
+            </p>
+            <p>
+              <Input name="login" placeholder="Login" />
+            </p>
+            <p>
+              <Input name="password" type="password" placeholder="Password" />
+            </p>
+            <p>
+              <Input
+                name="confirm_password"
+                type="password"
+                placeholder="Repeat Password"
+              />
+            </p>
+            <p>
+              <button type="submit">ok</button>
+            </p>
+          </Form>
+          <div className="formlinks">
+            <p>
+              <a href="">Forgot your password ?</a>
+            </p>
+          </div>
         </div>
-      </div>
+      )}
+      <Toaster />
     </>
   );
 };
